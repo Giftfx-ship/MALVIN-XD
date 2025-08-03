437da2db8a7c59b81fcec23a19b506f8c02c6e6b
Kingsley_khord <kingsleykiller8@gmail.com>
2025-08-03 16:52:46 +0300

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
+  SESSION_ID: "malvin~JykCSYzR#Mba4Fd3GLwNQjlo74_8U1Z3SqozAwRWiqK3JCk4Sj9k",  // add your Session Id make sure it starts with malvin~
   OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
